0659d1ba8de47945f5a3ff767e154210bc46b8c0
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-07-24 19:58:42 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,12 +13,12 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 var Games = [
-        "Sitemiz : evobot.site123.me",
-		"Yapımcım : ๖̶̶̶ۣۣۜۜ͜ζ͜͡MuradKerimli#9807",
-        `evo!yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`
+        "${prefix}yardım ",
+	"${client.guilds.size} sunucu + ${client.users.size} kullanıcı",
+        `ma*yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`
     ];
     setInterval(function() {
-        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
         client.user.setGame(Games[random], "https://www.twitch.tv/azeriiteammurad");
         }, 2 * 2500);
 };
