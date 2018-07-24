09444ba8a502c5c49453f92f45f952b978011000
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-07-24 21:52:12 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,5 +4,4 @@ var prefix = ayarlar.prefix;
 
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**! Buraya arkadaşlarını davet edebilir ve onlarla oyun oynayabilir, yeni arkadaşlar edinip onlarla oyunlar oynayıp sohbet edebilirsin!');
 };
