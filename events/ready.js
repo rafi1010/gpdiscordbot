49c257a2cd56530eb6e6089a195e24ea3f671422
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-07-24 19:53:16 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -11,14 +11,13 @@ var Games = {
 };
 setInterval(function() {
         var random = Math.floor(Math.random()*(Games.length-0+1)+0);
-        client.user.setGame(Games[random], "https://www.twitch.tv/azeriiteammurad");
+        client.user.setGame(Games[random]);
         }, 2 * 2500);
 
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(Games[random]);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
