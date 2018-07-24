a04490f6d3eadc163ad90b533c08b71e5a2bb1cc
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-07-24 19:43:23 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -15,10 +15,10 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);    
-	var Games = [
+var Games = [
         "Sitemiz : discord.io/GangstasParadise",
-	"Yapımcım : 【ǤⱣ】◉ Rafet Alperen¹⁹",
-        "`ma*yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`"
+		"Yapımcım : 【ǤⱣ】◉ Rafet Alperen¹⁹",
+        `ma*yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`
     ];
     setInterval(function() {
         var random = Math.floor(Math.random()*(Games.length-0+1)+0);
