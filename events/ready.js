c88fcf9a7eb1eaa8738ce6c4876e6afe8d819382
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-07-24 19:56:15 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,20 +4,21 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
-var Games = [
-	"Sitemiz : discord.io/GangstasParadise",
-	"Yapımcım : 【ǤⱣ】◉ Rafet Alperen¹⁹",
-	`ma*yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`
-];
-setInterval(function() {
-        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
-        client.user.setGame(Games[random]);
-        }, 2 * 2500);
 
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
+  client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+var Games = [
+        "Sitemiz : evobot.site123.me",
+		"Yapımcım : ๖̶̶̶ۣۣۜۜ͜ζ͜͡MuradKerimli#9807",
+        `evo!yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`
+    ];
+    setInterval(function() {
+        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+        client.user.setGame(Games[random], "https://www.twitch.tv/azeriiteammurad");
+        }, 2 * 2500);
 };
