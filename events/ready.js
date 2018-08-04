19ce63b9c2c20c2f309790e06ff9b1ca5f06bcf1
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-04 20:08:28 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -12,7 +12,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 var Games = [
-        "Sitemiz: discord.io/GangstasParadise ",
+        "Sitemiz: discord.gg/Fz9BMrj ",
 	"Yapımcım: 【ǤⱣ】◉ Rafet Alperen",
         `ma!yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`
     ];
