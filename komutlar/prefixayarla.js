dd410acfc3c518bcb05c5d12abcc1fb18c571f0d
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-09 03:42:44 +0300

    Update prefixayarla.js

--- komutlar/prefixayarla.js
+++ komutlar/prefixayarla.js
@@ -3,12 +3,14 @@ const db = require('quick.db');
 exports.run = (client, message, args) => {
 
   if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`**Afedersin, Gerekli Yetkin Bulunmamakta**`);
-
+  
   fs.writeFile('./serverdosyaları/guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
      	if (err) console.log(err)
 
   db.set(`prefix_${message.guild.id}`, args.join(' ')).then(i => {
       message.channel.send(`Prefix Başarıyla **${i}** Yapıldı`)
+
+	})
   })
 
 };
