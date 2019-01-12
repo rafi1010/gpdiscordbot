c686962526f1f7631b56d06f3667244c08208a2b
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2019-01-12 13:33:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
 const fs = require('fs');
+const weather = require('weather-js');
 const db = require('quick.db');
 const moment = require('moment');
 require('./util/eventLoader')(client);
