fb819db4c57972e412d80fc46db758d1bd3e75e1
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2020-11-07 04:32:35 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -18,6 +18,7 @@ var Games = [
     ];
     setInterval(function() {
 	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
-        client.user.setGame(Games[random], "https://www.twitch.tv/antiquary01");
+        client.user.setGame(Games[random]);
+	//client.user.setGame(Games[random], "https://www.twitch.tv/antiquary01");
         }, 2 * 2500);
 };
